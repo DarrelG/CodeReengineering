b99ce5efd5fbd9d3d6251df5cd235bba390a8bbe
ChristianS <christianzxz123@gmail.com>
2025-05-28 01:19:32 +0700

    Refactor Staff Class
    Merefactor dan enambahkan kelas ProfileData untuk merefactor Long Parameter List

---
+++ Project AOL Kelompok 5/Solutions/CarStore/src/carstore/ProfileData.java
@@ -0,0 +1,17 @@
+package carstore;
+
+public class ProfileData {
+    public String name;
+    public String address;
+    public String contactNumber;
+    public String email;
+    public int ID;
+
+    public ProfileData(String name, String address, String contactNumber, String email, int ID) {
+        this.name = name;
+        this.address = address;
+        this.contactNumber = contactNumber;
+        this.email = email;
+        this.ID = ID;
+    }
+}
--- Project AOL Kelompok 5/Solutions/CarStore/src/carstore/Staff.java
+++ Project AOL Kelompok 5/Solutions/CarStore/src/carstore/Staff.java
@@ -7,87 +7,61 @@ Summary of Staff :
 1-   Staff class represents  details of Empyees  
 */
 
+/*
+ * Smell code : Long parameter list
+ * Reason     : Parameter pada constructor telalu panjang.
+ * Solution   : Membuat objek ProfileData untuk mengurangi parameter pada constructor
+ * 
+ * Smell code : The Comments
+ * Reason     : Banyak komentar yang tidak penting
+ * Solution   : Menghapus comment dan mengganti penamaan variable agar lebih deskriptif
+ * 
+ * Smell code : Data Class
+ * Reason     : Ada Constructor tetapi ada setter
+ * Solution   : Menghapus setter pada class Staff
+ */
+
 public class Staff extends Profile {
-    
-    
-      //additional  atributes  staff  has 
     private String position;
     private double salary;
     private double retier;
 
-    
-    //constructor
-    public Staff(String name, String address, String contactNumber, String email, int ID,
-     String position,double salary,double retier) {
-        super(name, address, contactNumber, email, ID);
-        this.salary=salary;
-        this.retier=retier;
-        this.position=position ;
+    public Staff(ProfileData profileData, String position, double salary, double retier) {
+        super(profileData.name, profileData.address, profileData.contactNumber, profileData.email, profileData.ID);
+        this.salary = salary;
+        this.retier = retier;
+        this.position = position;
     }
 
-    
-    
-    /*
-    Seter and getter
-    Description: the purpose of this fuctions :- Set and get the value of the instance variable 
-    */
-    public void setPosition(String position){
-       this. position=position;}
-    
-    
-    public String getPosition(){
-      return position;}
-    
-    
-    public void setSalary(double salary ){
-     this.salary=salary;}
-    
-    
-    public double getSalary(){
-    return salary;}
-    
-    
-    public void setRetire(double retier ){
-    this.retier=retier;}
-    
-    
 
-    public Double getRetire(){
-    return retier;}
+    public String getPosition() {
+        return position;
+    }
 
+    public double getSalary() {
+        return salary;
+    }
 
+    public Double getRetire() {
+        return retier;
+    }
 
-    
-// Description:the purpose of this fuction :-  copy of the object on which it is called
+    @Override
+    public Profile clone() {
+        ProfileData profileData = new ProfileData(name, address, contactNumber, email, ID);
+        return new Staff(profileData, position, salary, retier);
+    }
 
-@Override
-public Profile clone(){
-    Staff f = new Staff(name,address,contactNumber,email,ID,position,
-            salary,retier);
-    return f;
+    @Override
+    public boolean equals(Object object) {
+        Staff staff = (Staff) object;
+        return super.equals(object) && staff.getPosition().equals(position)
+                && staff.getRetire() == retier && staff.getSalary() == salary;
+    }
+
+    @Override
+    public String toString() {
+        return (super.toString() + "\t" + position + "\t " + salary + "     " + retier);
+    }
+
 }
-
-
-// Description:the purpose of this fuction :-  compare the object on which it is called with its parmeters
-
-@Override
-public boolean equals(Object s){
-    Staff f=(Staff)s;
-        return super.equals(s)&&f.getPosition().equals(position)
-                &&f.getRetire()==retier&&f.getSalary()==salary;
-    }
-
-
-//Description: this function is type of override which used to show details of class
-
-@Override
-public String toString(){
-    return(super.toString()+"\t"+position+ "\t "+salary+"     "+retier
-            );
-}
-
- 
-    }
-
-
-
